17edce3c25a87d207d0b4761e1d4327a264745c0
Gytis Daujotas <gytdau@gmail.com>
2019-01-20 21:25:15 +0000

    Added tags

--- src/containers/NoteCard.js
+++ src/containers/NoteCard.js
@@ -7,6 +7,7 @@ export default (props) => (
       <div className="card">
         <div className="card-body">
           <h5 className="card-title" dangerouslySetInnerHTML={{ __html: props.note.name + (props.note.folder > 0 ? " <div class='size-indicator text-muted'>" + props.note.folder + "</div>" : "") }} />
+          {props.note.tags ? <div>{props.note.tags.map((tag) => <div className="tag" id={"tag-" + tag}>{tag}</div>)}</div> : null}
         </div>
       </div>
     </Link>
